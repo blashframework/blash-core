72073c630427d00b997a9e0e987e631a0fd6dced
Robert Trainor <blashframework@gmail.com>
2023-01-14 18:33:31 -0600

    Fixed load order for files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,6 +12,19 @@ lua54 'yes'
 ui_page 'html/ui.html'
 files { 'html/app.js', 'html/ui.html', 'html/app.css' }
 
-client_scripts { 'client/main.lua', 'client/*.lua' }
-server_scripts { '@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/*.lua' }
+client_scripts {
+    'client/main.lua',
+    'client/functions.lua',
+    'client/threads.lua',
+    'client/events.lua',
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+    'server/functions.lua',
+    'server/player.lua',
+    'server/events.lua',
+    'server/commands.lua',
+    'server/threads.lua'
+}
 shared_scripts { 'shared/*.lua', 'locales/en.lua', 'locales/*.lua' }
